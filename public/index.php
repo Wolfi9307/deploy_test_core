2df0d5292bb07ba450a5418c39ab8ec7aa822ab4
wolfi9307 <wolfi9307@gmail.com>
2019-07-14 04:26:14 +0300

    require path fix

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'view.php')) {
-	require __DIR__ . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'view.php';
+if (file_exists(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'view.php')) {
+	require __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'resources' . DIRECTORY_SEPARATOR . 'view.php';
 }
 
 echo "<h1>Core version one</h1>";
